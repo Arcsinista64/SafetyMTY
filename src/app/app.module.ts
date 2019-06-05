e95dcfd0e592af64252c5664c1ec6340ad00af49
arcsinista64 <ing.garciaruiz.9707@gmail.com>
2019-06-05 01:08:54 -0500

    Estancado en dropabbles y mostrar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MapComponent } from './components/map/map.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    MapComponent,
+    AsideComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
